54e26c557ca6d37b00a9ab8d6adece137257dba9
jzolo22 <jzolotarev@gmail.com>
2020-11-29 17:39:58 -0500

    commit tree ceb3969d771cf0f64ee2d24e247332dc45adf083

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 class Formatter {
-  //add static methods here
+    static capitalize(string) {
+      return string[0].toUpperCase() + string.slice(1)
+    }
+
+    static sanitize(string) {
+      return string.replace(/[^A-Za-z0-9-' ]+/g, '')
+    }
+
+    static titleize(string) {
+      return string[0].toUpperCase() + string.slice(1)
+    }
 }
